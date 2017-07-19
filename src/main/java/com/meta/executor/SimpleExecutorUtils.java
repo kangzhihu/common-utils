573d08680ed3df119b48903d2f03080e2b919d64
kangzhihu <kangzhihu@yupiao.com>
2017-07-19 14:56:04 +0800

    format

--- src/main/java/com/meta/executor/SimpleExecutorUtils.java
+++ src/main/java/com/meta/executor/SimpleExecutorUtils.java
@@ -6,97 +6,96 @@ import org.slf4j.LoggerFactory;
 import java.util.concurrent.*;
 
 public class SimpleExecutorUtils {
-	
-	private Logger log = LoggerFactory.getLogger(SimpleExecutorUtils.class);
 
-	private int corePoolSize = 1;
+    private Logger logger = LoggerFactory.getLogger(SimpleExecutorUtils.class);
 
-	private int maximumPoolSize = 10;
+    private int corePoolSize = 1;
 
-	private int keepAliveTime = 60 * 60;
+    private int maximumPoolSize = 10;
 
-	private TimeUnit timeUnit = TimeUnit.SECONDS;
+    private int keepAliveTime = 60 * 60;
 
-	private BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(100);
+    private TimeUnit timeUnit = TimeUnit.SECONDS;
 
-	private RejectedExecutionHandler handler = new ThreadPoolExecutor.DiscardPolicy();
+    private BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(100);
 
-	private ThreadPoolExecutor executor = null;
+    private RejectedExecutionHandler handler = new ThreadPoolExecutor.DiscardPolicy();
 
-	// private int produceTaskSleepTime = 60; //设置默认值
-	
-	private volatile static SimpleExecutorUtils singleton;
+    private ThreadPoolExecutor executor = null;
 
-	public static SimpleExecutorUtils getSingleton() {
-		if (singleton == null) {
-			synchronized (SimpleExecutorUtils.class) {
-				if (singleton == null) {
-					singleton = new SimpleExecutorUtils();
-				}
-			}
-		}
-		return singleton;
-	}
+    // private int produceTaskSleepTime = 60; //设置默认值
 
-	private SimpleExecutorUtils() {
-		init();
-	}
+    private volatile static SimpleExecutorUtils singleton;
 
-	private void init() {
-		executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize,
-				keepAliveTime, timeUnit, workQueue, handler);
-		Runtime.getRuntime().addShutdownHook(new Thread(){
+    public static SimpleExecutorUtils getSingleton() {
+        if (singleton == null) {
+            synchronized (SimpleExecutorUtils.class) {
+                if (singleton == null) {
+                    singleton = new SimpleExecutorUtils();
+                }
+            }
+        }
+        return singleton;
+    }
+
+    private SimpleExecutorUtils() {
+        init();
+    }
+
+    private void init() {
+        executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize,
+                keepAliveTime, timeUnit, workQueue, handler);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
                 if (executor != null) {
                     executor.shutdown();
                 }
             }
-		});
-	}
-	
-	public String addTask(Runnable task) {
-		if(task != null) {
-			try {
-				executor.execute(task);
-				return "success";
-			}
-			catch (Exception e1) {
-				log.error(e1.getMessage());
-				return "error";
-			}
-		} else {
-			return "error";
-		}
-	}
+        });
+    }
 
-	/**
-	 * 检查当前线程池是否有空闲线程用于执行任务
-	 * 
-	 * @return false：没有空闲线程, true：有空闲线程
-	 */
-	public boolean checkActive() {
-		// 活跃线程数
-		int activeNum = SimpleExecutorUtils.getSingleton().getExecutor().getActiveCount();
-		// 当前池中的线程数
-		int curNum = SimpleExecutorUtils.getSingleton().getExecutor().getPoolSize();
-		// 允许最大线程数
-		int maxNum = SimpleExecutorUtils.getSingleton().getExecutor().getMaximumPoolSize();
-		log.info("线程池中的活跃线程数:" + activeNum + ", 当前池中的线程数:" + curNum + ", 允许最大线程数:" + maxNum);
-		// 只要最大线程数大于当前活跃线程数，即可执行任务
-		if (maxNum <= activeNum) {
-			return false;
-		} else {
-			return true;
-		}
-	}
+    public String addTask(Runnable task) {
+        if (task != null) {
+            try {
+                executor.execute(task);
+                return "success";
+            } catch (Exception e1) {
+                logger.error(e1.getMessage());
+                return "error";
+            }
+        } else {
+            return "error";
+        }
+    }
 
-	public ThreadPoolExecutor getExecutor() {
-		return executor;
-	}
+    /**
+     * 检查当前线程池是否有空闲线程用于执行任务
+     *
+     * @return false：没有空闲线程, true：有空闲线程
+     */
+    public boolean checkActive() {
+        // 活跃线程数
+        int activeNum = SimpleExecutorUtils.getSingleton().getExecutor().getActiveCount();
+        // 当前池中的线程数
+        int curNum = SimpleExecutorUtils.getSingleton().getExecutor().getPoolSize();
+        // 允许最大线程数
+        int maxNum = SimpleExecutorUtils.getSingleton().getExecutor().getMaximumPoolSize();
+        logger.info("线程池中的活跃线程数:" + activeNum + ", 当前池中的线程数:" + curNum + ", 允许最大线程数:" + maxNum);
+        // 只要最大线程数大于当前活跃线程数，即可执行任务
+        if (maxNum <= activeNum) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
-	public void setExecutor(ThreadPoolExecutor executor) {
-		this.executor = executor;
-	}
+    public ThreadPoolExecutor getExecutor() {
+        return executor;
+    }
+
+    public void setExecutor(ThreadPoolExecutor executor) {
+        this.executor = executor;
+    }
 
 }
