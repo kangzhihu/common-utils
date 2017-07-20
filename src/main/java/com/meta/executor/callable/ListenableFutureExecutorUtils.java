02b28433e8f6c49f3a1253dc5c1077aba99d583d
kangzhihu <kangzhihu@yupiao.com>
2017-07-20 10:46:00 +0800

    listenableFuture Util

---
+++ src/main/java/com/meta/executor/callable/ListenableFutureExecutorUtils.java
@@ -0,0 +1,91 @@
+package com.meta.executor.callable;
+
+import com.google.common.util.concurrent.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.*;
+
+public class ListenableFutureExecutorUtils {
+
+    private static transient Logger logger = LoggerFactory.getLogger(ListenableFutureExecutorUtils.class);
+
+    private int corePoolSize = 1;
+
+    private int maximumPoolSize = 10;
+
+    private int keepAliveTime = 60 * 60;
+
+    private TimeUnit timeUnit = TimeUnit.SECONDS;
+
+    private BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<Runnable>(100);
+
+    private RejectedExecutionHandler handler = new ThreadPoolExecutor.DiscardPolicy();
+
+    private ListeningExecutorService service = null;
+
+    private ThreadPoolExecutor executor = null;
+
+    private volatile static ListenableFutureExecutorUtils singleton;
+
+    public static ListenableFutureExecutorUtils getSingleton() {
+            if (singleton == null) {
+                synchronized (ListenableFutureExecutorUtils.class) {
+                    if (singleton == null) {
+                        singleton = new ListenableFutureExecutorUtils();
+                    }
+                }
+            }
+        return singleton;
+    }
+
+    private ListenableFutureExecutorUtils() {
+        init();
+    }
+
+    private void init() {
+        executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize,
+                keepAliveTime, timeUnit, workQueue, handler);
+        service = MoreExecutors.listeningDecorator(executor);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                if (executor != null) {
+                    executor.shutdown();
+                }
+            }
+        });
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                if (service != null) {
+                    service.shutdown();
+                }
+            }
+        });
+    }
+
+    /**
+     * 提交任务,完成后将调用对应的监听器
+     * @param task
+     * @param callback
+     * @param <T>
+     * @return
+     */
+    public <T> void addTask(Callable<T> task,FutureCallback<T> callback) {
+        if (task != null && callback != null) {
+            ListenableFuture<T> listenableFuture = service.submit(task);
+            Futures.addCallback(listenableFuture,callback);
+        }
+    }
+
+    public ThreadPoolExecutor getExecutor() {
+        return executor;
+    }
+
+    public void setExecutor(ThreadPoolExecutor executor) {
+        this.executor = executor;
+    }
+
+
+}
