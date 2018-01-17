4413f914f4ee70bb257bbf1137e10f3e54a0d551
kangzhihu <kangzhihu@gmail.com>
2018-01-17 14:13:32 +0800

    Create MapUtils.java

---
+++ src/main/java/com/meta/common/MapUtils.java
@@ -0,0 +1,84 @@
+
+    public class MapUtils{
+    
+      /**
+     * 获取对象List的Map<unionField,Bean>格式的数据
+     * @param beanList
+     * @param unionField 唯一属性名
+     * @param <K> unionField值类型
+     *  @param <V> Bean类型
+     * @return
+     */
+    public static <K, V> Map<K, V> getBeanListMap(List<V> beanList, String unionField) {
+        Map<K, V> map = new HashMap<>();
+        if (beanList == null || beanList.isEmpty()) {
+            return map;
+        }
+        beanList.stream().forEach((bean) -> {
+            try {
+                Map tmap = BeanUtils.describe(bean);
+                if (tmap.get(unionField) != null) {
+                    map.put((K) tmap.get(unionField), bean);
+                }
+            } catch (Exception e) {
+                logger.error("unexpected exception on bean operation!");
+            }
+        });
+        return map;
+    }
+       /**
+     * 获取简单list bean同key多value类型map
+     * @param beanList
+     * @param filed bean属性字段
+     * @param value bean属性字段
+     * @param <K>
+     * @param <V>
+     * @param <E>
+     * @return
+     */
+    public static <K,V,E> Multimap<K, V> getMapFromBeanList(List<E> beanList, String filed, String value) {
+        Multimap<K, V> beanMultimap = ArrayListMultimap.create();
+        if(beanList == null || beanList.isEmpty()){
+            return beanMultimap;
+        }
+        for(E bean:beanList){
+            Map tmap = null;
+            try {
+                tmap = BeanUtils.describe(bean);
+            } catch (Exception e) {
+                return ArrayListMultimap.create();
+            }
+            if (tmap.get(filed) != null) {
+                beanMultimap.put((K) tmap.get(filed),(V) tmap.get(value));
+            }
+        }
+        return beanMultimap;
+    }
+
+    /**
+     * 获取简单list bean同key多value类型map
+     * @param beanList
+     * @param filed bean属性字段
+     * @param <K>
+     * @param <E>
+     * @return
+     */
+    public static <K,E> Multimap<K, E> getMapFromBeanList(List<E> beanList, String filed) {
+        Multimap<K, E> beanMultimap = ArrayListMultimap.create();
+        if(beanList == null || beanList.isEmpty()){
+            return beanMultimap;
+        }
+        for(E bean:beanList){
+            Map tmap = null;
+            try {
+                tmap = BeanUtils.describe(bean);
+            } catch (Exception e) {
+                return ArrayListMultimap.create();
+            }
+            if (tmap.get(filed) != null) {
+                beanMultimap.put((K) tmap.get(filed),bean);
+            }
+        }
+        return beanMultimap;
+    }
+    }
