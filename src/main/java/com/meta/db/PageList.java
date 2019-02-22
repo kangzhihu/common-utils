dfc31a51da40e7a5330c3fde04e5561d8c8dcfc9
kangzhihu <kangzhihu@gmail.com>
2019-02-22 14:34:57 +0800

    Create PageList.java

---
+++ src/main/java/com/meta/db/PageList.java
@@ -0,0 +1,98 @@
+package com.meta.db;
+
+public class PageList extends ArrayList {
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = 3257568390985103409L;
+    private Paginator paginator;
+
+    /**
+     * 创建一个<code>PageList</code>。
+     */
+    public PageList() {
+        paginator = new Paginator();
+    }
+
+    /**
+     * 创建<code>PageList</code>，并将指定<code>Collection</code>中的内容复制到新的list中。
+     *
+     * @param c 要复制的<code>Collection</code>
+     */
+    public PageList(Collection c) {
+        this(c, null);
+    }
+
+    /**
+     * 创建<code>PageList</code>，并将指定<code>Collection</code>中的内容复制到新的list中。
+     *
+     * @param c 要复制的<code>Collection</code>
+     */
+    public PageList(Collection c, Paginator paginator) {
+        super(c);
+        this.paginator = (paginator == null) ? new Paginator()
+                                             : paginator;
+    }
+
+    /**
+     * 取得分页器，可从中取得有关分页和页码的所有信息。
+     *
+     * @return 分页器对象
+     */
+    public Paginator getPaginator() {
+        return paginator;
+    }
+
+    /**
+     * 设置分页器。
+     *
+     * @param paginator 要设置的分页器对象
+     */
+    public void setPaginator(Paginator paginator) {
+        if (paginator != null) {
+            this.paginator = paginator;
+        }
+    }
+
+    /**
+     * @deprecated use getPaginator() instead
+     */
+    public int getPageSize() {
+        return paginator.getItemsPerPage();
+    }
+
+    /**
+     * @deprecated use getPaginator() instead
+     */
+    public int getTotalItem() {
+        return paginator.getItems();
+    }
+
+    /**
+     * @deprecated use getPaginator() instead
+     */
+    public int getTotalPage() {
+        return paginator.getPages();
+    }
+
+    /**
+     * @deprecated use getPaginator() instead
+     */
+    public void setPageSize(int i) {
+        paginator.setItemsPerPage(i);
+    }
+
+    /**
+     * @deprecated use getPaginator() instead
+     */
+    public void setTotalItem(int i) {
+        paginator.setItems(i);
+    }
+
+    /**
+     * @deprecated use getPaginator() instead
+     */
+    public void setTotalPage(int i) {
+        setTotalItem(i * getPageSize());
+    }
+}
